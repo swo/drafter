2e3434198c69398ac243c89239975dd14b41074e
swo <swo@alum.mit.edu>
2020-01-12 17:01:35 -0500

    package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="drafter",
+      version="0.1",
+      description="Smart draft management",
+      url="http://github.com/swo/drafter",
+      author="Scott Olesen",
+      author_email="swo@alum.mit.edu",
+      license="MIT",
+      packages=["drafter"],
+      zip_safe=False)
